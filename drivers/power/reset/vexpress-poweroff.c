2ed3975887b59733e47d1e70f80eb4e68fc8fe2c
Volodymyr Zhdanov <wight554@gmail.com>
2021-06-22 12:20:19 +0300

    [DNM]: power: remove mojito-r-oss changes we don't need

--- drivers/power/reset/vexpress-poweroff.c
+++ drivers/power/reset/vexpress-poweroff.c
@@ -9,7 +9,6 @@
  * GNU General Public License for more details.
  *
  * Copyright (C) 2012 ARM Limited
- * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include <linux/delay.h>
@@ -151,7 +150,6 @@ static struct platform_driver vexpress_reset_driver = {
 	.driver = {
 		.name = "vexpress-reset",
 		.of_match_table = vexpress_reset_of_match,
-		.suppress_bind_attrs = true,
 	},
 };
 
